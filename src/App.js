7974ee2494b4b3829fcd6016774a0724fcae0eb9
kmometria <kerry@ometria.com>
2020-04-04 17:32:18 +0100

    add Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
 import Board from "./components/Board/Board"
+import {JoinForm} from "./components/Form/Form"
 import './App.css';
 
 
@@ -8,6 +9,7 @@ import './App.css';
 function App() {
   const socket = socketIOClient('http://127.0.0.1:4001')
   const [socData, setSocData] = useState('hoya init')
+  const [nameAndInitials, setNameAndInitials] = useState(null)
 
   useEffect(() => {
     socket.on('chat message', (msg)=> setSocData(msg));
@@ -19,8 +21,8 @@ function App() {
 
   return (
     <div className="App">
+     {!nameAndInitials && <JoinForm returnValues={setNameAndInitials}/>}
      <Board />
-
       <p onClick={()=> emit()}>
         Socket <code>{socData}</code> 
       </p>
---
+++ src/components/Form/Form.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import './form.css'
+
+export const JoinForm = ({returnValues}) => {
+  const [name, setName] = useState("");
+  const [firstInital, setFirstInital] = useState("");
+  const [lastInital, setLastInital] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleChange = (e) => {
+    const target = e.target;
+
+    if (target.name == "name") setName(target.value);
+    if (target.name == "firstInital") setFirstInital(target.value);
+    if (target.name == "lastInital") setLastInital(target.value);
+
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!name || !firstInital || !lastInital) setErrorMsg('You missed a couple field(s) 😅. Please fill them out.')
+    else {
+      setErrorMsg('')
+      returnValues({name, firstInital, lastInital})
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <p className="errorMsg">{errorMsg}</p>
+        <label>
+          <span className="label">Name:</span>
+          <input
+            name="name"
+            type="text"
+            placeholder="Kerrynator"
+            value={name}
+            onChange={(e) => handleChange(e)}
+            maxLength="30"
+            require={true}
+          />
+        </label>
+        <label>
+          <span className="label">Initials:</span>
+          <div className="initialsContainer">
+            <input
+              name="firstInital"
+              type="text"
+              placeholder="k"
+              value={firstInital}
+              onChange={(e) => handleChange(e)}
+              maxLength="1"
+              require={true}
+            />
+            <input
+              name="lastInital"
+              type="text"
+              placeholder="M"
+              value={lastInital}
+              onChange={(e) => handleChange(e)}
+              maxLength="1"
+              require={true}
+            />
+          </div>
+        </label>
+        <button type="submit">Join Game</button>
+      </form>
+    </div>
+  );
+};
+
+export default { JoinForm };
